7edd456d2055cbfe4afe75df7079cf485de40d01
vaishu-15 <vaishali15yadav@gmail.com>
2024-05-01 12:45:39 +0530

    keys maded

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ import reducers from './reducers';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['auth','Login','Home','updateUser']
+  whitelist: ['auth']
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
